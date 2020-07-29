1ac36730b55446ce2a8eadecae9672316dbf088c
Arian <arian.kulmer@web.de>
2020-07-29 16:01:55 +0200

    Introduce anti flicker mode in LiveDisplay
    Change-Id: Iba7387ff16d463d087c9a38bced90b6c1bd503b1
    
    [jhonboy121]: @hide setting keys
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I0198695ae8d0c6d60dd18f3a067fb44625c294b3
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6607,6 +6607,13 @@ public final class Settings {
         public static final String LIVE_DISPLAY_HINTED = "live_display_hinted";
 
         /**
+         * Anti flicker
+         * 0 = 0ff, 1 = on
+         * @hide
+         */
+        public static final String DISPLAY_ANTI_FLICKER = "display_anti_flicker";
+
+        /**
          * Enable statusbar double tap gesture on to put device to sleep
          * 0 = 0ff, 1 = on
          * @hide
@@ -6774,6 +6781,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_ADJUSTMENT);
             PRIVATE_SETTINGS.add(DISPLAY_PICTURE_ADJUSTMENT);
             PRIVATE_SETTINGS.add(LIVE_DISPLAY_HINTED);
+            PRIVATE_SETTINGS.add(DISPLAY_ANTI_FLICKER);
         }
 
         /**
