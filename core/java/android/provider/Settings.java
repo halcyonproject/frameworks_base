7c434545a6f1ed7ac997ff014ad69a93d4c485c6
Danny Lin <danny@kdrag0n.dev>
2022-04-10 18:35:11 -0700

    SystemUI: Add API for runtime taskbar config
    Allow Launcher3 to override the taskbar state at runtime based on user
    preference.
    
    Change-Id: Ic2616ff974e98e03edbcdbdf677fa40b40b37abb
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6662,6 +6662,11 @@ public final class Settings {
          * the setting value. See an example above.
          */
 
+	/**
+         * @hide
+         */
+        public static final String ENABLE_TASKBAR = "enable_taskbar";
+
         /**
          * Whether the torch launch gesture when the screen is off should be enabled.
          * @hide
