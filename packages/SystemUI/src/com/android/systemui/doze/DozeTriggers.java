19e4909515f65c49989353e4bf30edd156437e94
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-27 16:42:56 +0300

    Partially revert "Assume sensors perform prox check"
    This commit partially reverts f40bd8fbb65c896c824fe3f1a5be857bbe8ae281
    ("Assume sensors perform prox check").
    
    Some devices do not have proximity gated sensors, and still require
    gating in software.
    Partially revert this commit to bring back the ability for sensors to
    be proximity gated in software.
    
    Change-Id: Ic3d5c6e98d1767623ace4ee4eea1c52606fabd4a

--- packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -306,7 +306,8 @@ public class DozeTriggers implements DozeMachine.Part {
     }
 
     @VisibleForTesting
-    void onSensor(int pulseReason, float screenX, float screenY, float[] rawValues) {
+    void onSensor(int pulseReason, boolean sensorPerformedProxCheck,
+            float screenX, float screenY, float[] rawValues) {
         boolean isDoubleTap = pulseReason == DozeLog.REASON_SENSOR_DOUBLE_TAP;
         boolean isTap = pulseReason == DozeLog.REASON_SENSOR_TAP;
         boolean isPickup = pulseReason == DozeLog.REASON_SENSOR_PICKUP;
@@ -323,11 +324,11 @@ public class DozeTriggers implements DozeMachine.Part {
                     mMachine.getState(),
                     pulseReason);
         } else if (isLongPress) {
-            requestPulse(pulseReason, true /* alreadyPerformedProxCheck */,
+            requestPulse(pulseReason, sensorPerformedProxCheck /* alreadyPerformedProxCheck */,
                     null /* onPulseSuppressedListener */);
         } else if (isWakeOnReach || isQuickPickup) {
             if (isWakeDisplayEvent) {
-                requestPulse(pulseReason, true /* alreadyPerformedProxCheck */,
+                requestPulse(pulseReason, sensorPerformedProxCheck /* alreadyPerformedProxCheck */,
                         null /* onPulseSuppressedListener */);
             }
         } else {
@@ -363,7 +364,7 @@ public class DozeTriggers implements DozeMachine.Part {
                 } else {
                     mDozeHost.extendPulse(pulseReason);
                 }
-            }, true /* alreadyPerformedProxCheck */, pulseReason);
+            }, sensorPerformedProxCheck /* alreadyPerformedProxCheck */, pulseReason);
         }
 
         if (isPickup && !shouldDropPickupEvent()) {
