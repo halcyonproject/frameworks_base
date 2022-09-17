d84e0a5acf6833918f92251e5bf86a19fced214f
Pranav Vashi <neobuddy89@gmail.com>
2022-09-17 12:20:38 +0530

    ThemeOverlayController: Remove repeated monet check
    Change-Id: I6608df48c1a89a87eaf08d9f267e0a83e9bbd3d1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -536,11 +536,6 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                 },
                 UserHandle.USER_ALL);
 
-        // All wallpaper color and keyguard logic only applies when Monet is enabled.
-        if (!mIsMonetEnabled) {
-            return;
-        }
-
         mUserTracker.addCallback(mUserTrackerCallback, mMainExecutor);
         mConfigurationController.addCallback(mConfigurationListener);
         mDeviceProvisionedController.addCallback(mDeviceProvisionedListener);
