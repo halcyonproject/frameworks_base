807e05c8068699088064ac1fbc42b61b96f86fe4
someone5678 <nemui3353@gmail.com>
2023-05-31 22:33:20 +0900

    [SQUASH] base: Hide ADB and developer setting enable status [1/2]
    * Some apps check whether ADB or developer setting is enabled
    * Return 0 to selected apps when those apps getInt
      ADB_ENABLED, ADB_WIFI_ENABLED, DEVELOPMENT_SETTINGS_ENABLED
    * Code is adapted and stripped down from cutout force full screen
    
    Ref:
    https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
    https://github.com/yaap/frameworks_base/commit/232260f192effc9fb21a54eb444601179b24a26d
    
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    base: HideDevStatus: move to getIntForUser to cover more scenarios
    
    * Adapt to current project as we want to set status individually per-app
      for case apps require ADB or Dev settings for their functionality
    
    e.g. SystemUI Tuner by Zachary Wander
    https://play.google.com/store/apps/details?id=com.zacharee1.systemuituner&hl=en_US&gl=US
    
    Ref:
    https://github.com/RisingTechOSS/android_frameworks_base/commit/77be7be42060dc5119c6dccef874c9a439aed210
    
    Change-Id: Ibbf92c7b1f1181d2508cb22ff2fc2017f9e59706
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    base: HideDeveloperStatus: Query all apps installed by any user [1/2]
    
    * And set value for all user
    
    Change-Id: Ibd2a5dfab832e6bdc6d074ce41401f4905602152
    
    base: HideDeveloperStatusUtils: Avoid NPE with mApps
    
    * https://paste.evolution-x.org/P9Lj8g.
    
    * Avoid NPE
    
    From: someone5678 <someone5678@users.noreply.github.com>
    fixup! base: Hide ADB and developer setting enable status [1/2]
    * Clean-up and handle exceptions
    
    Change-Id: I4b1ca5f3c39fe043ce79c302d84ab81dc2a0829d
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -108,6 +108,8 @@ import android.widget.Editor;
 import com.android.internal.annotations.GuardedBy;
 import com.android.internal.util.Preconditions;
 
+import com.android.internal.util.halcyon.HideDeveloperStatusUtils;
+
 import java.io.IOException;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -4644,6 +4646,9 @@ public final class Settings {
         @UnsupportedAppUsage
         public static int getIntForUser(ContentResolver cr, String name, int def,
                 @CanBeCURRENT @UserIdInt int userId) {
+            if (HideDeveloperStatusUtils.shouldHideDevStatus(cr, cr.getPackageName(), name)) {
+                return 0 /* Disabled */;
+            }
             String v = getStringForUser(cr, name, userId);
             return parseIntSettingWithDefault(v, def);
         }
@@ -4676,6 +4681,9 @@ public final class Settings {
         public static int getIntForUser(ContentResolver cr, String name,
                 @CanBeCURRENT @UserIdInt int userId)
                 throws SettingNotFoundException {
+            if (HideDeveloperStatusUtils.shouldHideDevStatus(cr, cr.getPackageName(), name)) {
+                return 0 /* Disabled */;
+            }
             String v = getStringForUser(cr, name, userId);
             return parseIntSetting(v, name);
         }
@@ -7883,6 +7891,9 @@ public final class Settings {
         @UnsupportedAppUsage
         public static int getIntForUser(ContentResolver cr, String name, int def,
                 @CanBeCURRENT @UserIdInt int userId) {
+            if (HideDeveloperStatusUtils.shouldHideDevStatus(cr, cr.getPackageName(), name)) {
+                return 0 /* Disabled */;
+            }
             String v = getStringForUser(cr, name, userId);
             return parseIntSettingWithDefault(v, def);
         }
@@ -7914,6 +7925,9 @@ public final class Settings {
         public static int getIntForUser(ContentResolver cr, String name,
                 @CanBeCURRENT @UserIdInt int userId)
                 throws SettingNotFoundException {
+            if (HideDeveloperStatusUtils.shouldHideDevStatus(cr, cr.getPackageName(), name)) {
+                return 0 /* Disabled */;
+            }
             String v = getStringForUser(cr, name, userId);
             return parseIntSetting(v, name);
         }
@@ -13598,6 +13612,13 @@ public final class Settings {
          */
         @Readable
         public static final String HALCYON_SPOOFED_APPS = "halcyon_spoofed_apps";
+
+        /**
+         * Control whether to hide ADB and Developer settings enable status.
+         * @hide
+         */
+        @Readable
+        public static final String HIDE_DEVELOPER_STATUS = "hide_developer_status";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
@@ -19761,6 +19782,9 @@ public final class Settings {
          * or not a valid integer.
          */
         public static int getInt(ContentResolver cr, String name, int def) {
+            if (HideDeveloperStatusUtils.shouldHideDevStatus(cr, cr.getPackageName(), name)) {
+                return 0 /* Disabled */;
+            }
             String v = getString(cr, name);
             return parseIntSettingWithDefault(v, def);
         }
@@ -19785,6 +19809,9 @@ public final class Settings {
          */
         public static int getInt(ContentResolver cr, String name)
                 throws SettingNotFoundException {
+            if (HideDeveloperStatusUtils.shouldHideDevStatus(cr, cr.getPackageName(), name)) {
+                return 0 /* Disabled */;
+            }
             String v = getString(cr, name);
             return parseIntSetting(v, name);
         }
