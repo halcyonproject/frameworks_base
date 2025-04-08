a28a71f3d7e756b7e2df60420103a462bcf7290c
Chandru S <chandruis@google.com>
2025-04-07 19:47:26 -0700

    Force behind and notification scrim alpha to be 0 when the device is unlocking.
    For dreaming -> unlocked transition:
     - mExpansionAffectsAlpha is false while ScrimController applyState is invoked, it early returns with the behind and notif alphas set to 0.45ish from UNLOCKED scrim state
     - This CL handles this scenario
    
    For keyguard -> unlocked transition:
     - mExpansionAffectsAlpha is false initially and ScrimController starts applying UNLOCKED state alphas (0.45ish)
     - CentralSurfaces#updateScrimController gets invoked, keyguard showing is now false and it sets mExpansionAffectsAlpha to true
     - ScrimController#onUnlockAnimationFinished is then invoked which invokes applyAndDispatchState which re-applies UNLOCKED scrim state
     - mExpansionAffectsAlpha is now true, early return doesn't happen in applyState
     - behindScrimAlpha and notifScrimAlpha get reset to 0 later in the applyState method.
    
    Fixes: 406364098
    Test: verified manually,
      1. setup fingerprint on a power button FP device
      2. Enable screensaver
      3. Go to screensaver
      4. Touch the power button to authenticate while on the screensaver
      5. Device unlocks and goes to launcher without leaving the behind scrim on the screen.
    
    Flag: com.android.systemui.notification_shade_blur
    Change-Id: I6898a47069550bc5305dc8b8650f132668fcd998
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -1008,6 +1008,10 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         assertAlphasValid();
 
         if (!mExpansionAffectsAlpha) {
+            if (Flags.notificationShadeBlur() && mState == ScrimState.UNLOCKED) {
+                mBehindAlpha = 0.0f;
+                mNotificationsAlpha = 0.0f;
+            }
             return;
         }
 
