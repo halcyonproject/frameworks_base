1e9e7bbd536f88936ec0440c168bf566742ed0e2
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-05-24 23:08:51 +0530

    SystemUI: Filter out duplicate notification icons on statusbar
    Updated for clean architecture on 15+.
    
    Change-Id: I77c01bef82bd62e5aafb470a201dea556da6fcf8

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/icon/domain/interactor/NotificationIconsInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/icon/domain/interactor/NotificationIconsInteractor.kt
@@ -42,6 +42,7 @@ import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 
 /** Domain logic related to notification icons. */
 class NotificationIconsInteractor
@@ -231,6 +232,12 @@ constructor(
                     showAodPromoted = false,
                 )
             }
+            .map { notifs ->
+                notifs
+                    .filter { it.statusBarIcon != null }
+                    .distinctBy { it.statusBarIcon!!.toString() }
+                    .toSet()
+            }
             .flowOn(bgContext)
 }
 
@@ -252,6 +259,12 @@ constructor(
                     showDismissed = false,
                     showRepliedMessages = false,
                 )
+            }
+            .map { notifs ->
+                notifs
+                    .filter { it.statusBarIcon != null }
+                    .distinctBy { it.statusBarIcon!!.toString() }
+                    .toSet()
             }
             .flowOn(bgContext)
 }
