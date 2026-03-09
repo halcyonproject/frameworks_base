3af2a6ab53124ddea865a2534fb0ecbb127bfbeb
Ghosuto <clash.raja10@gmail.com>
2026-03-09 12:52:51 +0000

    SystemUI: Add interaction boost for QS/shade animations
    - based on https://github.com/AOSPA/android_frameworks_base/commit/dfef0f9bfa04a4acf44782e04ff9a1e183560409
    
    Uses PowerManagerInternal.setPowerBoost() via LocalServices,
    matching the approach used in DisplayPolicy for system gesture flings
    
    Goal - reduce jank and improve responsiveness
    
    Boost points:
    - onTrackingStarted: when finger drag begins
    - flingToHeight: when fling animator is committed
    - flingToHeight onAnimationEnd/Cancel: fling settle and interrupts
    - springBack onAnimationEnd: overshoot spring settle
    - maybeVibrateOnOpening: shade open start
    - onPanelTrackingStarted: open panel
    
    Co-authored-by: Jake Weinstein <jake@aospa.co>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -63,10 +63,12 @@ import android.graphics.Rect;
 import android.graphics.Region;
 import android.graphics.RenderEffect;
 import android.graphics.Shader;
+import android.hardware.power.Boost;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.os.PowerManagerInternal;
 import android.os.Trace;
 import android.provider.Settings;
 import android.util.IndentingPrintWriter;
@@ -99,6 +101,7 @@ import com.android.keyguard.ActiveUnlockConfig;
 import com.android.keyguard.KeyguardUnfoldTransition;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.dagger.KeyguardStatusBarViewComponent;
+import com.android.server.LocalServices;
 import com.android.systemui.DejankUtils;
 import com.android.systemui.Dumpable;
 import com.android.systemui.Flags;
@@ -1529,6 +1532,7 @@ public final class NotificationPanelViewController implements
         }
         setAnimator(animator);
         animator.start();
+        boostInteraction((int) animator.getDuration());
     }
 
     @VisibleForTesting
@@ -2132,6 +2136,7 @@ public final class NotificationPanelViewController implements
     }
 
     private void onTrackingStarted() {
+        boostInteraction(700);
         endClosing();
         mShadeRepository.setLegacyShadeTracking(true);
         if (mTrackingStartedListener != null) {
@@ -2503,6 +2508,13 @@ public final class NotificationPanelViewController implements
         }
     }
 
+    private void boostInteraction(int durationMs) {
+        PowerManagerInternal pmi = LocalServices.getService(PowerManagerInternal.class);
+        if (pmi != null) {
+            pmi.setPowerBoost(Boost.INTERACTION, durationMs);
+        }
+    }
+
     private class ShadeHeadsUpTrackerImpl implements ShadeHeadsUpTracker {
         @Override
         public void addTrackingHeadsUpListener(Consumer<ExpandableNotificationRow> listener) {
@@ -2848,6 +2860,7 @@ public final class NotificationPanelViewController implements
             mIsExpandingOrCollapsing = true;
             mQsController.onExpandingStarted(mQsController.getFullyExpanded());
         }
+        boostInteraction(700);
     }
 
     void notifyExpandingFinished() {
@@ -2908,6 +2921,7 @@ public final class NotificationPanelViewController implements
     private void maybeVibrateOnOpening(boolean openingWithTouch) {
         if (mVibrateOnOpening && mBarState != KEYGUARD && mBarState != SHADE_LOCKED) {
             if (!openingWithTouch || !mHasVibratedOnOpen) {
+                boostInteraction(500);
                 performHapticFeedback(HapticFeedbackConstants.GESTURE_START);
                 mHasVibratedOnOpen = true;
                 mShadeLog.v("Vibrating on opening, mHasVibratedOnOpen=true");
@@ -3099,6 +3113,7 @@ public final class NotificationPanelViewController implements
         });
         setAnimator(animator);
         animator.start();
+        boostInteraction(500);
     }
 
     @VisibleForTesting
